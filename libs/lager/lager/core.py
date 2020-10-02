9963bfc3ee1d78c16a882d59043cd83c96744a85
jesse <jessekrubin@gmail.com>
2020-10-02 12:58:38 -0700

    updated to poetry 1.1.0

--- libs/lager/lager/core.py
+++ libs/lager/lager/core.py
@@ -1,18 +1,88 @@
 # -*- coding: utf-8 -*-
 """Python lager brewed by a loguru"""
 import asyncio
-
 from functools import wraps
 from time import time
 from typing import Union
+import atexit as _atexit
+import sys as _sys
 
+from loguru import _defaults
+from loguru._handler import Handler
+from loguru._logger import Core as _Core
+from loguru._logger import Logger as _Logger
+from jsonbourne import json
 from loguru import logger
-
 from lager.const import LOG_LEVELS
 
 
 __all__ = ['loglevel', 'flog', 'handlers', 'logger', 'log', 'LOG', 'ln', 'LN']
 
+try:
+    import orjson
+    def _serialize_record(text, record):
+        exception = record["exception"]
+
+        if exception is not None:
+            exception = {
+                "type"     : None if exception.type is None else exception.type.__name__,
+                "value"    : exception.value,
+                "traceback": bool(record["exception"].traceback),
+                }
+
+        serializable = {
+            "text"  : text,
+            "record": {
+                "elapsed"  : {
+                    "repr"   : record["elapsed"],
+                    "seconds": record["elapsed"].total_seconds(),
+                    },
+                "exception": exception,
+                "extra"    : record["extra"],
+                "file"     : {
+                    "name": record["file"].name,
+                    "path": record["file"].path
+                    },
+                "function" : record["function"],
+                "level"    : {
+                    "icon": record["level"].icon,
+                    "name": record["level"].name,
+                    "no"  : record["level"].no,
+                    },
+                "line"     : record["line"],
+                "message"  : record["message"],
+                "module"   : record["module"],
+                "name"     : record["name"],
+                "process"  : {
+                    "id"  : record["process"].id,
+                    "name": record["process"].name
+                    },
+                "thread"   : {
+                    "id"  : record["thread"].id,
+                    "name": record["thread"].name
+                    },
+                "time"     : {
+                    "repr"     : record["time"],
+                    "timestamp": record["time"].timestamp()
+                    },
+                },
+            }
+        return orjson.dumps(serializable) + "\n"
+
+    Handler._serialize_record = staticmethod(_serialize_record)
+except ModuleNotFoundError:
+    pass
+
+logger = _Logger(_Core(), None, 0, False, False, False, False, True, None, {})
+if _defaults.LOGURU_AUTOINIT and _sys.stderr:
+    logger.add(_sys.stderr)
+
+_atexit.register(logger.remove)
+########################
+## Non patching stuff ##
+########################
+
+# Aliases
 logger.t = logger.trace
 logger.d = logger.debug
 logger.i = logger.info
@@ -62,8 +132,10 @@ def flog(funk=None, level="debug", enter=True, exit=True):
 
     """
 
+
     def _flog(funk):
         name = funk.__name__
+
 
         @wraps(funk)
         def _flog_decorator(*args, **kwargs):
@@ -75,7 +147,7 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     args,
                     kwargs,
-                )
+                    )
             ti = time()
             result = funk(*args, **kwargs)
             tf = time()
@@ -86,8 +158,9 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     result,
                     tf - ti,
-                )
+                    )
             return result
+
 
         @wraps(funk)
         async def _flog_decorator_async(*args, **kwargs):
@@ -99,7 +172,7 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     args,
                     kwargs,
-                )
+                    )
             ti = time()
             result = await funk(*args, **kwargs)
             tf = time()
@@ -110,12 +183,14 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     result,
                     tf - ti,
-                )
+                    )
             return result
+
 
         if asyncio.iscoroutinefunction(funk) or asyncio.iscoroutine(funk):
             return _flog_decorator_async
         return _flog_decorator
+
 
     return _flog(funk) if funk else _flog
 
