9e6c8239ee106d600bf82c377983b43c73c7d220
jessekrubin <jessekrubin@gmail.com>
2020-10-29 21:51:35 +0000

    minor bump for python 39

--- libs/lager/lager/_version.py
+++ libs/lager/lager/_version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """`lager` version"""
-__version__ = "0.11.1"
+__version__ = "0.12.0"
 VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH = [int(el) for el in __version__.split('.')]
 VERSION_INFO = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
