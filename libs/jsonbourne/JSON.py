9739ed6d59579f48b907be91817e019962f73cdd
jessekrubin <jessekrubin@gmail.com>
2020-12-03 18:26:08 -0800

    fixed bug with the version meta data in jsonbournes aux JSON file

--- libs/jsonbourne/JSON.py
+++ libs/jsonbourne/JSON.py
@@ -2,15 +2,8 @@
 """JSON callable module ~ jsonbourne"""
 import sys
 
-from jsonbourne import json
+from jsonbourne import json, __version__
 from jsonbourne._import import import_json
-from jsonbourne._version import (
-    VERSION_INFO,
-    VERSION_MAJOR,
-    VERSION_MINOR,
-    VERSION_PATCH,
-    __version__,
-)
 from jsonbourne.core import (
     JSON,
     JsonDict,
@@ -30,6 +23,7 @@ parse = JSON.parse
 json_lib = JSON.json_lib
 
 __all__ = [
+    "__version__",
     "json",  # json compat lib
     # core
     "JsonObjMutableMapping",
@@ -46,10 +40,4 @@ __all__ = [
     "loads",
     "json_lib",
     "parse",
-    # Version stuff
-    "VERSION_MAJOR",
-    "VERSION_MINOR",
-    "VERSION_PATCH",
-    "VERSION_INFO",
-    "__version__",
 ]
--- libs/jsonbourne/jsonbourne/_meta.py
+++ libs/jsonbourne/jsonbourne/_meta.py
@@ -2,6 +2,6 @@
 """Package metadata/info"""
 
 __title__ = 'jsonbourne'
-__version__ = '0.13.0'
+__version__ = '0.14.0'
 __license__ = 'MIT'
 __description__ = 'EZPZ JSON'
