bcd4180e0d3f99f9e1ac6be0573066f4a72b0bcf
jesse <jessekrubin@gmail.com>
2021-01-06 15:57:32 -0800

    jsonbourne httpx wrapper

---
+++ libs/jsonbourne/jsonbourne/httpx.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Jsonbourne wrapper around httpx clients -- lets you do response.JSON()"""
+
+from typing import Any
+
+from httpx import *
+from httpx import __all__ as __httpx_all__
+from jsonbourne import JSON
+
+
+__all__ = [*__httpx_all__]
+
+def _JSON(self, **kwargs: Any) -> Any:
+    return JSON(self.json())
+
+Response.JSON = _JSON
