b94a00f74fa3f83927bd2f52a4fe5b90b174a003
jesse <jessekrubin@gmail.com>
2021-10-29 11:36:55 -0700

    dumb mistake

--- libs/aiopen/aiopen/__init__.py
+++ libs/aiopen/aiopen/__init__.py
@@ -2,12 +2,11 @@
 """`aiopen` ~ Async version of python's built in open -- based on aiofiles"""
 from aiopen._meta import __version__
 from aiopen.core import aiopen
+from funkify.core import funkify
 
-# from funkify.core import funkify
+funkify(aiopen, name="aiopen")
 
-# funkify(aiopen, name="aiopen")
-
-__all__ = [
+__all__ = (
     "__version__",
     "aiopen",
-]
+)
