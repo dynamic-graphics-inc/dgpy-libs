20e54a273ade25d1a38860c740a0907fe6dd2cdf
jessekrubin <jessekrubin@gmail.com>
2021-11-17 18:00:27 -0800

    added logging aliases

--- libs/lager/lager/logging.py
+++ libs/lager/lager/logging.py
@@ -2,9 +2,129 @@
 """Lager/Loguru + std logging"""
 import logging
 
+from logging import (
+    BASIC_FORMAT,
+    CRITICAL,
+    DEBUG,
+    ERROR,
+    FATAL,
+    INFO,
+    NOTSET,
+    WARN,
+    WARNING,
+    BufferingFormatter,
+    FileHandler,
+    Filter,
+    Formatter,
+    Handler,
+    Logger,
+    LoggerAdapter,
+    LogRecord,
+    NullHandler,
+    StreamHandler,
+    addLevelName,
+    basicConfig,
+    captureWarnings,
+    critical,
+    debug,
+    disable,
+    error,
+    exception,
+    fatal,
+    getLevelName,
+    getLogger,
+    getLoggerClass,
+    getLogRecordFactory,
+    info,
+    lastResort,
+    log,
+    makeLogRecord,
+    raiseExceptions,
+    setLoggerClass,
+    setLogRecordFactory,
+    shutdown,
+    warn,
+    warning,
+)
 from typing import Any, Dict, List
 
 from lager.core import LOG, loglevel
+
+__all__ = (
+    'BASIC_FORMAT',
+    'BufferingFormatter',
+    'CRITICAL',
+    'DEBUG',
+    'ERROR',
+    'FATAL',
+    'FileHandler',
+    'Filter',
+    'Formatter',
+    'Handler',
+    'INFO',
+    'LogRecord',
+    'Logger',
+    'LoggerAdapter',
+    'NOTSET',
+    'NullHandler',
+    'StreamHandler',
+    'WARN',
+    'WARNING',
+    'addLevelName',
+    'add_level_name',
+    'basicConfig',
+    'basic_config',
+    'captureWarnings',
+    'capture_warnings',
+    'critical',
+    'debug',
+    'disable',
+    'error',
+    'exception',
+    'fatal',
+    'getLevelName',
+    'getLogRecordFactory',
+    'getLogger',
+    'getLoggerClass',
+    'get_level_name',
+    'get_log_record_factory',
+    'get_logger',
+    'get_logger_class',
+    'info',
+    'lastResort',
+    'last_resort',
+    'log',
+    'makeLogRecord',
+    'make_log_record',
+    'raiseExceptions',
+    'raise_exceptions',
+    'setLogRecordFactory',
+    'setLoggerClass',
+    'set_log_record_factory',
+    'set_logger_class',
+    'shutdown',
+    'warn',
+    'warning',
+    # lager.logging members
+    'StdLoggingHandler',
+    'loggers_dict',
+    'intercept',
+    'intercept_all',
+)
+
+# logging snake_case aliases
+add_level_name = addLevelName
+basic_config = basicConfig
+capture_warnings = captureWarnings
+get_level_name = getLevelName
+get_log_record_factory = getLogRecordFactory
+get_logger = getLogger
+get_logger_class = getLoggerClass
+last_resort = lastResort
+make_log_record = makeLogRecord
+raise_exceptions = raiseExceptions
+set_log_record_factory = setLogRecordFactory
+set_logger_class = setLoggerClass
 
 
 class StdLoggingHandler(logging.Handler):
