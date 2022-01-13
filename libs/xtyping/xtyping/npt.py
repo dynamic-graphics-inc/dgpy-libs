d6189592f2fde40bdea6a5efb957a29d4b3141a3
jesse <jessekrubin@gmail.com>
2022-01-12 20:46:51 -0800

    numpy.typing for xtyping

---
+++ libs/xtyping/xtyping/npt.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""import numpy.typing as npt"""
+from typing import TYPE_CHECKING, Any
+
+import numpy as np
+
+from typing_extensions import TypeAlias
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
+
+    ArrayLike: TypeAlias = npt.ArrayLike
+    DTypeLike: TypeAlias = npt.DTypeLike
+    NBitBase: TypeAlias = npt.NBitBase
+    NDArray: TypeAlias = npt.NDArray
+else:
+    ArrayLike = Any
+    DTypeLike = np.dtype
+    NBitBase = Any
+    NDArray = np.ndarray
+
+__all__ = ("ArrayLike", "DTypeLike", "NBitBase", "NDArray")
