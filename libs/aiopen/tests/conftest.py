f7b93c7e692dc3f0aac1312fe6694d3bfb667b0e
jesse <jessekrubin@gmail.com>
2022-01-14 13:57:51 -0800

    almost done

--- libs/aiopen/tests/conftest.py
+++ libs/aiopen/tests/conftest.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-@pytest.fixture(params=[
-    pytest.param(('asyncio', {'use_uvloop': True}), id='asyncio+uvloop'),
-    pytest.param(('asyncio', {'use_uvloop': False}), id='asyncio'),
-    # pytest.param(('trio', {'restrict_keyboard_interrupt_to_checkpoints': True}), id='trio')
-])
+_trio_backend = pytest.param(
+    ("trio", {"restrict_keyboard_interrupt_to_checkpoints": True}), id="trio"
+)
+
+
+@pytest.fixture(
+    params=[
+        pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio+uvloop"),
+        pytest.param(("asyncio", {"use_uvloop": False}), id="asyncio"),
+    ]
+)
 def anyio_backend(request):
     return request.param
