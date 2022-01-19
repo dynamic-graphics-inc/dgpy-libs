04922e33ba2190922b7e691b0239d6ff10f6d294
jesse <jessekrubin@gmail.com>
2022-01-19 15:23:14 -0800

    anyio asyncify

---
+++ libs/asyncify/asyncify/_anyio.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from functools import partial, wraps
+
+from anyio import CapacityLimiter as CapacityLimiter, run as run, to_thread as to_thread
+
+from xtyping import Awaitable, Callable, Optional, ParamSpec, TypeVar
+
+__all__ = (
+    "run",
+    "asyncify",
+)
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def asyncify(
+    funk: Callable[P, R],
+    *,
+    cancellable: bool = False,
+    limiter: Optional[CapacityLimiter] = None,
+) -> Callable[P, Awaitable[R]]:
+    """asyncify decorator/wrapper that for use w/ anyio"""
+
+    @wraps(funk)
+    async def _asyncified(*args: P.args, **kwargs: P.kwargs) -> R:
+        partial_f = partial(funk, *args, **kwargs)
+        return await to_thread.run_sync(
+            partial_f, cancellable=cancellable, limiter=limiter
+        )
+
+    return _asyncified
