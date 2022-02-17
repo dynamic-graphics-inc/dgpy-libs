44832f42810d4e9f205bade33e3f727563e6f08b
jesse <jessekrubin@gmail.com>
2022-02-17 12:19:59 -0800

    stupid httpx thing

--- libs/lager/lager/dev.py
+++ libs/lager/lager/dev.py
@@ -2,22 +2,25 @@
 
 from typing import Any
 
-import httpx
+try:
+    import httpx
 
-_SINKS = []
+    _SINKS = []
 
 
-class HttpxSink(object):
-    def __init__(self, url: str, *args: Any, **kwargs: Any) -> None:
-        self.url = url
-        self.client = httpx.AsyncClient(*args, **kwargs)
-        _SINKS.append(self)
+    class HttpxSink(object):
+        def __init__(self, url: str, *args: Any, **kwargs: Any) -> None:
+            self.url = url
+            self.client = httpx.AsyncClient(*args, **kwargs)
+            _SINKS.append(self)
 
-    async def __call__(self, msg: Any) -> None:
-        httpx.post(self.url, data={"msg": msg})
+        async def __call__(self, msg: Any) -> None:
+            httpx.post(self.url, data={"msg": msg})
 
-    async def handle(self, message: Any) -> None:
-        await self.client.post(url=self.url, data={"msg": message})
+        async def handle(self, message: Any) -> None:
+            await self.client.post(url=self.url, data={"msg": message})
 
-    async def await_delete_channels(self) -> None:
-        await self.client.aclose()
+        async def await_delete_channels(self) -> None:
+            await self.client.aclose()
+except ImportError:
+    pass
