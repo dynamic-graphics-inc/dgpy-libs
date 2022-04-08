406fda8eff03ca520904af014aa5a3e133cd09c0
jessekrubin <jessekrubin@gmail.com>
2022-04-08 09:36:41 -0700

    jsonbourne version update

--- scripts/pypi-versions.sh
+++ scripts/pypi-versions.sh
@@ -3,12 +3,12 @@
 DGPY_LIBS=(aiopen  asyncify  fmts  funkify  h5  jsonbourne  lager  listless  requires  shellfish  xtyping)
 
 function pypi_versions(){
-    echo $(pip index versions ${1} | grep -i available | cat f.txt | cut -d' ' -f 3- | jq -R 'split(", ")')
+    echo $(pip index versions ${1} | grep -i available | cut -d' ' -f 3- | jq -R 'split(", ")')
 }
 
 function pypi_versions_obj(){
     pkgname=$1
-    vobj=$(pip index versions ${1} 2>/dev/null | grep -i available | cat f.txt | cut -d' ' -f 3- | jq -R "split(\", \") | {\"$pkgname\": .}")
+    vobj=$(pip index versions ${pkgname} 2>/dev/null | grep -i available | cut -d' ' -f 3- | jq -R "split(\", \") | {\"$pkgname\": .}")
     echo $vobj
 }
 
@@ -20,9 +20,12 @@ function pypi_versions_obj_task(){
 function main(){
     mkdir -p temp
     for i in "${DGPY_LIBS[@]}";do
-        pypi_versions_obj_task "$i" > "temp/$i.json" &
+        echo "$i"
+        # thingy=$(pypi_versions_obj $i)
+        # echo $thingy
+        pypi_versions_obj "$i" > "temp/$i.json"
     done;
-    wait;
+    # wait;
     echo "Pkg versions downloaded!"
     cat temp/* | jq -s add | jq -S > "_meta/pypi-pkg-versions.json"
 
