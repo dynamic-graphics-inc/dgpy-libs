daa41b20974e6b34a092df0d9a0af897e454faa8
jessekrubin <jessekrubin@gmail.com>
2022-05-12 10:22:23 -0700

    scripts update

---
+++ scripts/poetry-update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DGPY_LIBS_REPO_ROOT=$(dirname "$SCRIPTS_DIR")
+
+jq -c -r '.[]' ${DGPY_LIBS_REPO_ROOT}/_meta/topo.json | while read i; do
+    echo "$i"
+    LIB_DIRPATH="${DGPY_LIBS_REPO_ROOT}/libs/${i}"
+    echo "Updating ${i} ~ ${LIB_DIRPATH}"
+    cd ${LIB_DIRPATH}
+    poetry update
+done
+
+cd ${DGPY_LIBS_REPO_ROOT}
+poetry update
+
+
