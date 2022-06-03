ac1a9a609f436e891ad5d5f1561b5c8952ce2c99
jesse <jessekrubin@gmail.com>
2022-06-02 19:33:46 -0700

    going to bump shellfish

--- libs/shellfish/shellfish/__init__.py
+++ libs/shellfish/shellfish/__init__.py
@@ -5,10 +5,11 @@ from __future__ import annotations
 from funkify import funkify
 from shellfish import dotenv, fs, process, sh
 from shellfish._meta import __version__
+from shellfish.process import env
 
 funkify(sh.do, key="shellfish.sh")
 funkify(sh.do, key="shellfish")
 
 ps = process
 
-__all__ = ("__version__", "dotenv", "fs", "process", "ps", "sh")
+__all__ = ("__version__", "dotenv", "env", "fs", "process", "ps", "sh")
