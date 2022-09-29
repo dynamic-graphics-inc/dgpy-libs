b73a05e9e6969adeea522d507063178beafc8660
jesse <jessekrubin@gmail.com>
2022-09-28 18:53:13 -0700

    I think this is good to go...

--- libs/dgpylibs/dgpylibs/__init__.py
+++ libs/dgpylibs/dgpylibs/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """Dynamic Graphics Python libraries"""
 
-from dgpylibs._meta import __version__
+# try:
+#     from jsonbourne.trydantic import dataclass
+# except ImportError:
+#     from dataclasses import dataclass
+from dataclasses import dataclass
 
 import aiopen
 import asyncify
@@ -14,7 +18,6 @@ import listless
 import requires
 import shellfish
 import xtyping
-
 from aiopen import __version__ as __aiopen_version__
 from asyncify import __version__ as __asyncify_version__
 from fmts import __version__ as __fmts_version__
@@ -27,10 +30,7 @@ from requires import __version__ as __requires_version__
 from shellfish import __version__ as __shellfish_version__
 from xtyping import __version__ as __xtyping_version__
 
-try:
-    from jsonbourne.trydantic import dataclass
-except ImportError:
-    from dataclasses import dataclass
+from dgpylibs._meta import __version__
 
 __all__ = (
     "__version__",
@@ -74,6 +74,47 @@ LIBS = (
 
 
 @dataclass(frozen=True)
+class DgpyLibMetadata:
+    title: str
+    description: str
+    pkgroot: str
+    version: str
+
+    @property
+    def __title__(self) -> str:
+        return self.title
+
+    @property
+    def __description__(self) -> str:
+        return self.description
+
+    @property
+    def __pkgroot__(self) -> str:
+        return self.pkgroot
+
+    @property
+    def __version__(self) -> str:
+        return self.version
+
+
+@dataclass(frozen=True)
+class DgpyLibsMetadata:
+    """dgpy-libs env info"""
+
+    aiopen: DgpyLibMetadata
+    asyncify: DgpyLibMetadata
+    fmts: DgpyLibMetadata
+    funkify: DgpyLibMetadata
+    h5: DgpyLibMetadata
+    jsonbourne: DgpyLibMetadata
+    lager: DgpyLibMetadata
+    listless: DgpyLibMetadata
+    requires: DgpyLibMetadata
+    shellfish: DgpyLibMetadata
+    xtyping: DgpyLibMetadata
+
+
+@dataclass(frozen=True)
 class DgpyLibsVersions:
     """dgpy-libs env info"""
 
@@ -89,6 +130,75 @@ class DgpyLibsVersions:
     shellfish: str = None
     xtyping: str = None
 
+
+dgpylibs_metadata = DgpyLibsMetadata(
+    aiopen=DgpyLibMetadata(
+        title=aiopen._meta.__title__,
+        description=aiopen._meta.__description__,
+        pkgroot=aiopen._meta.__pkgroot__,
+        version=aiopen._meta.__version__,
+    ),
+    asyncify=DgpyLibMetadata(
+        title=asyncify._meta.__title__,
+        description=asyncify._meta.__description__,
+        pkgroot=asyncify._meta.__pkgroot__,
+        version=asyncify._meta.__version__,
+    ),
+    fmts=DgpyLibMetadata(
+        title=fmts._meta.__title__,
+        description=fmts._meta.__description__,
+        pkgroot=fmts._meta.__pkgroot__,
+        version=fmts._meta.__version__,
+    ),
+    funkify=DgpyLibMetadata(
+        title=funkify._meta.__title__,
+        description=funkify._meta.__description__,
+        pkgroot=funkify._meta.__pkgroot__,
+        version=funkify._meta.__version__,
+    ),
+    h5=DgpyLibMetadata(
+        title=h5._meta.__title__,
+        description=h5._meta.__description__,
+        pkgroot=h5._meta.__pkgroot__,
+        version=h5._meta.__version__,
+    ),
+    jsonbourne=DgpyLibMetadata(
+        title=jsonbourne._meta.__title__,
+        description=jsonbourne._meta.__description__,
+        pkgroot=jsonbourne._meta.__pkgroot__,
+        version=jsonbourne._meta.__version__,
+    ),
+    lager=DgpyLibMetadata(
+        title=lager._meta.__title__,
+        description=lager._meta.__description__,
+        pkgroot=lager._meta.__pkgroot__,
+        version=lager._meta.__version__,
+    ),
+    listless=DgpyLibMetadata(
+        title=listless._meta.__title__,
+        description=listless._meta.__description__,
+        pkgroot=listless._meta.__pkgroot__,
+        version=listless._meta.__version__,
+    ),
+    requires=DgpyLibMetadata(
+        title=requires._meta.__title__,
+        description=requires._meta.__description__,
+        pkgroot=requires._meta.__pkgroot__,
+        version=requires._meta.__version__,
+    ),
+    shellfish=DgpyLibMetadata(
+        title=shellfish._meta.__title__,
+        description=shellfish._meta.__description__,
+        pkgroot=shellfish._meta.__pkgroot__,
+        version=shellfish._meta.__version__,
+    ),
+    xtyping=DgpyLibMetadata(
+        title=xtyping._meta.__title__,
+        description=xtyping._meta.__description__,
+        pkgroot=xtyping._meta.__pkgroot__,
+        version=xtyping._meta.__version__,
+    ),
+)
 
 dgpylibs_info = DgpyLibsVersions(
     aiopen=__aiopen_version__,
--- libs/dgpylibs/dgpylibs/__main__.py
+++ libs/dgpylibs/dgpylibs/__main__.py
@@ -2,20 +2,26 @@
 """pkg entry ~ `python -m dgpylibs`"""
 import sys
 
+from dgpylibs import dgpylibs_metadata
 from dgpylibs._meta import __pkgroot__, __title__, __version__
 
 sys.stdout.write(
-    f"package: {__title__}\nversion: {__version__}\npkgroot: {__pkgroot__}\n"
+    f"package: {__title__}\nversion: {__version__}\npkgroot: {__pkgroot__}\n\n"
 )
 
-from aiopen import __version__ as __aiopen_metadata__
-from asyncify import __version__ as __asyncify_metadata__
-from fmts import __version__ as __fmts_metadata__
-from funkify import __version__ as __funkify_metadata__
-from h5 import __version__ as __h5_metadata__
-from jsonbourne import __version__ as __jsonbourne_metadata__
-from lager import __version__ as __lager_metadata__
-from listless import __version__ as __listless_metadata__
-from requires import __version__ as __requires_metadata__
-from shellfish import __version__ as __shellfish_metadata__
-from xtyping import __version__ as __xtyping_metadata__
+
+sys.stdout.write(
+    "\n\n".join(
+        (
+            "\n".join(
+                f"    {el}"
+                for el in (
+                    f"package: {v.__title__}",
+                    f"version: {v.__version__}",
+                    f"pkgroot: {v.__pkgroot__}",
+                )
+            )
+            for v in dgpylibs_metadata.__dict__.values()
+        )
+    )
+)
