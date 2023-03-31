dfdaf1cb482d1b09309acbf25a2701c17a2f7dd9
jessekrubin <jessekrubin@gmail.com>
2023-03-30 19:22:54 -0700

    add numpy version

--- libs/h5/h5/__main__.py
+++ libs/h5/h5/__main__.py
@@ -7,12 +7,18 @@ from h5._meta import __pkgroot__, __title__, __version__
 
 
 def main() -> None:
+    import numpy as np
+
+    from h5py import __version__ as __h5py_version__
+
     sys.stdout.write(
         json.dumps(
             {
                 "package": __title__,
                 "version": __version__,
                 "pkgroot": __pkgroot__,
+                "h5py_version": __h5py_version__,
+                "numpy_version": np.__version__,
             }
         )
     )
