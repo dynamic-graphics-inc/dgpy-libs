a8da31c54f4b31e300e7042f2298ade5980b528f
jessekrubin <jessekrubin@gmail.com>
2023-04-28 11:12:02 -0700

    all sorted

--- libs/requires/tests/test_shed.py
+++ libs/requires/tests/test_shed.py
@@ -1,7 +1,18 @@
-def test_shed_expoerts():
-    import requires.shed as requires_shed
+import requires.shed as requires_shed
 
-    assert requires_shed.__all__ == tuple(sorted(set(requires_shed.__all__)))
+
+def test_shed_exports():
     shed_requirements = [el for el in dir(requires_shed) if el.startswith("requires_")]
-    for shed_requirement in shed_requirements:
-        assert shed_requirement in requires_shed.__all__
+    requires_shed_all_set = set(requires_shed.__all__)
+    missing_from_all = [
+        el for el in shed_requirements if el not in requires_shed_all_set
+    ]
+    if missing_from_all:
+        raise AssertionError(f"MISSING from __all__: {str(tuple(missing_from_all))}")
+
+
+def test_all_sorted():
+    expected = tuple(sorted(set(requires_shed.__all__)))
+    assert requires_shed.__all__ == tuple(
+        sorted(set(requires_shed.__all__))
+    ), f"__all__ should be sorted -- __all__ = {expected}"
