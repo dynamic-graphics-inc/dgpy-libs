b0fd95b1138d9f6857f4da7bbb6dde6a4b59d551
jessekrubin <jessekrubin@gmail.com>
2023-07-17 09:37:53 -0700

    update jsonbourne to 0.26.0

--- libs/jsonbourne/jsonbourne/__about__.py
+++ libs/jsonbourne/jsonbourne/__about__.py
@@ -5,4 +5,4 @@ __all__ = ("__title__", "__description__", "__pkgroot__", "__version__")
 __title__ = "jsonbourne"
 __description__ = "EZPZ JSON"
 __pkgroot__ = __file__.replace("__about__.py", "").rstrip("/\\")
-__version__ = "0.25.2"
+__version__ = "0.26.0"
