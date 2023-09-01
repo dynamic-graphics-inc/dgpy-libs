49f5f43c3a56c79b093d94114ebd88ca3bfd92b9
Dan Costello <dan@dgi.com>
2023-09-01 10:50:43 -0400

    trying another way to close

--- libs/aiopen/tests/aiofiles_anyio/test_simple.py
+++ libs/aiopen/tests/aiofiles_anyio/test_simple.py
@@ -56,7 +56,7 @@ async def test_serve_small_bin_file_sync(
 
     assert payload == file_content
 
-    await server.wait_closed()
+    await server.close()
 
 
 @pytest.mark.anyio()
@@ -99,4 +99,4 @@ async def test_serve_small_bin_file(
     payload = await reader.read()
 
     assert payload == file_content
-    await server.wait_closed()
+    await server.close()
