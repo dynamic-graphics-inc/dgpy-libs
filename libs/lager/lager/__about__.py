1f66698cc15ff2883334cba7b821dca8a6742b78
jesse <jessekrubin@gmail.com>
2024-02-01 14:34:13 -0800

    lager hot fix (#890)

--- libs/lager/lager/__about__.py
+++ libs/lager/lager/__about__.py
@@ -7,4 +7,4 @@ __all__ = ("__title__", "__description__", "__pkgroot__", "__version__")
 __title__ = "lager"
 __description__ = "EZ-PZ logging based on loguru"
 __pkgroot__ = __file__.replace("__about__.py", "").rstrip("/\\")
-__version__ = "0.18.1"
+__version__ = "0.18.2"
