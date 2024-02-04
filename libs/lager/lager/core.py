c041126b5eb5f4dc06ce5cdc4fa1ca8936488ef0
Dan Costello <dan@dgi.com>
2024-02-04 14:22:00 -0500

    this is the way

--- libs/lager/lager/core.py
+++ libs/lager/lager/core.py
@@ -9,8 +9,9 @@ from functools import wraps
 from time import time
 from typing import Any, Callable, Dict, Optional, TypeVar, Union
 
-from loguru import Record, logger
+from loguru import logger as logger
 from loguru._handler import Handler
+from loguru._defaults import Record
 
 from lager.const import LOG_LEVELS
 
