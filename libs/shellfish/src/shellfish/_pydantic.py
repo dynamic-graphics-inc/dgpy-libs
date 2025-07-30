83149498faa579b92f176d986eb43a2ecb7986a1
jessekrubin <jessekrubin@gmail.com>
2025-07-30 15:04:53 -0700

    move off of json basemodel

--- libs/shellfish/src/shellfish/_pydantic.py
+++ libs/shellfish/src/shellfish/_pydantic.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
 """shelfish._pydantic"""
 
+from __future__ import annotations
+
+from typing import Any
+
 from pydantic import BaseModel, ConfigDict
 
 __all__ = ("_ShellfishBaseModel", "shellfish_base_model_config")
 
 shellfish_base_model_config: ConfigDict = {
     "extra": "forbid",
-    "arbitrary_types_allowed": True,
-    "populate_by_name": True,
-    "use_enum_values": True,
-    "validate_default": True,
 }
 
 
-class _ShellfishBaseModel(BaseModel): ...
+class _ShellfishBaseModel(BaseModel):
+    """Pydantic shellfish base model"""
+
+    model_config = shellfish_base_model_config
+
+    def __json_interface__(self) -> dict[str, Any]:
+        """Return the JSON interface for this model."""
+        return self.model_dump(
+            exclude_none=True,
+            exclude_unset=True,
+            exclude_defaults=True,
+        )
