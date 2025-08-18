1068f5cf1272eb2252362d002630be4f5f22c691
jessekrubin <jessekrubin@gmail.com>
2025-08-18 14:37:01 -0700

    bump all versions

--- libs/dgpytest/src/dgpytest/__about__.py
+++ libs/dgpytest/src/dgpytest/__about__.py
@@ -7,4 +7,4 @@ __all__ = ("__description__", "__pkgroot__", "__title__", "__version__")
 __title__ = "dgpytest"
 __description__ = "dgpytest = dgpy + pytest"
 __pkgroot__ = __file__.replace("__about__.py", "").rstrip("/\\")
-__version__ = "0.0.5"
+__version__ = "0.0.6"
