2c91c53de4c4e0e255c9f053fefed798f33436ea
jessekrubin <jessekrubin@gmail.com>
2025-08-25 18:39:01 -0700

    fix jsonbourne

--- libs/jsonbourne/src/jsonbourne/core.py
+++ libs/jsonbourne/src/jsonbourne/core.py
@@ -33,7 +33,7 @@ from jsonbourne import jsonlib
 if TYPE_CHECKING:
     from pathlib import Path
 
-    from pydantic import GetCoreSchemaHandler
+    from pydantic import CoreSchema, GetCoreSchemaHandler
 
 JsonPrimitiveT = TypeVar("JsonPrimitiveT", str, int, float, None)
 JsonObjT = TypeVar("JsonObjT", bound="JsonObj[Any]")
@@ -1053,11 +1053,12 @@ class JsonObj(MutableMapping[str, _VT], Generic[_VT]):
         return cls(val)
 
     @classmethod
-    def __get_type_validator__(
+    def __get_pydantic_core_schema__(
         cls, source_type: Any, handler: GetCoreSchemaHandler
-    ) -> Iterator[Callable[[Any], Any]]:
-        """Return the JsonObj validator functions"""
-        yield cls.validate_type
+    ) -> CoreSchema:
+        from pydantic_core import core_schema
+
+        return core_schema.no_info_plain_validator_function(cls.validate_type)
 
 
 class JsonDict(JsonObj[_VT], Generic[_VT]):
