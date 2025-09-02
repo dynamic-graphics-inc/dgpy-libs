a413d4acbb4bbee9c0041ddea32c7f134545f09a
jessekrubin <jessekrubin@gmail.com>
2025-09-02 14:40:26 -0700

    xtyping modernize

--- libs/xtyping/src/xtyping/npt.py
+++ libs/xtyping/src/xtyping/npt.py
@@ -3,11 +3,9 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, TypeAlias
 
 import numpy as np
-
-from typing_extensions import TypeAlias
 
 if TYPE_CHECKING:
     import numpy.typing as npt
