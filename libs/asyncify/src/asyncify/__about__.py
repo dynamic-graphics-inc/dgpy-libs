451d20cc77ae82bfc9f526011b7eb74550d115d2
jessekrubin <jessekrubin@gmail.com>
2025-10-15 14:33:33 -0700

    update internal versions

--- libs/asyncify/src/asyncify/__about__.py
+++ libs/asyncify/src/asyncify/__about__.py
@@ -7,4 +7,4 @@ __all__ = ("__description__", "__pkgroot__", "__title__", "__version__")
 __title__ = "asyncify"
 __description__ = "sync 2 async"
 __pkgroot__ = __file__.replace("__about__.py", "").rstrip("/\\")
-__version__ = "0.12.0"
+__version__ = "0.12.1"
